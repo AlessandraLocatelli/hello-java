1a335068fc045277859f0e5a4c6dd2bbe4675200
Ales <alocatelli798@gmail.com>
2023-07-11 12:49:21 +0200

    java first exercise example

---
+++ src/org/java/lessons/HelloJava.java
@@ -0,0 +1,12 @@
+package org.java.lessons;
+
+public class HelloJava {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("This is my first Java program!");
+
+    }
+
+}
